eaad369d4512e7bec4683ebe8a95968859f4df30
DavidLiu321 <>
2021-04-13 06:35:06 +0000

    Added core main interaction without questions

--- main.pl
+++ main.pl
@@ -1,6 +1,35 @@
 :- include('writer.pl').
 
-start :-
-  write('Please upload a valid profile file'), flush_output(current_output),
-  load,
+mainf :-
+    write('Please make sure you have uploaded a valid profile, tasks file, and busy times file! \n Press Enter when ready.\n'), flush_output(current_output),
+    read_sq(Ready1),
+    catch(load, Err, recoverLoad),
+    askForInfo,
+    write('Creating a schedule for you...\n'), flush_output(current_output),
+    createSlotsWrapper, % not sure if this is the start of the algorithm
+    write('Schedule created, writing schedule to CSV...\n'), flush_output(current_output),
+    %writeToCSV. - wait for jack to create correct assertions for assignments.
+    write('Schedule written to output.csv!\n'), flush_output(current_output).
+
+recoverLoad :-
+    write('One of the given profile, tasks file, or busy times file is invalid\n'), flush_output(current_output),
+    write('Please upload a valid file for each 3! Press Enter when you are ready.\n'), flush_output(current_output),
+    read_sq(Ready),
+    catch(load, Err,  recoverLoad).
+
+askForInfo :-
+    write('Would you like to know some info about your profile? (y/n) \n'), flush_output(current_output),
+    read_sq(YesOrNo),
+    checkYes(YesOrNo) -> processQuestions(YesOrNo); 
+                    (write('Invalid response. Please answer either yes (y) or no (n) '), askForInfo).
+
+checkYes(y).
+checkYes(n).
+
+processQuestions(n) :-
+    write('Understood. Moving forward to create a schedule.\n'), flush_output(current_output).
+processQuestions(y) :-
+    write('What would you like to know?.\n (1) Which tasks Take longer than X hours? \n (2) When am I available on X date?'), flush_output(current_output).
+
   
+  %cd desktop/CPSC312/312Scheduler
